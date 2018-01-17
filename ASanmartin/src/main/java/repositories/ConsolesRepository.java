82fcffd1127ca7acab3d39324a4e2ebf2908a2cc
Alex <isanmartin8a@gmail.com>
2018-01-17 19:08:24 +0100

    Añadiendo los repositorios.
    Signed-off-by: Alex <isanmartin8a@gmail.com>

--- ASanmartin/src/main/java/repositories/ConsolesRepository.java
+++ ASanmartin/src/main/java/repositories/ConsolesRepository.java
@@ -1,5 +1,147 @@
 package repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import connections.AbstractConnection;
+import models.Console;
+
 public class ConsolesRepository {
+	
+	private static final String jdbcUrl = "jdbc:h2:file:./src/main/resources/test;INIT=RUNSCRIPT FROM 'classpath:scripts/Console.sql'";
+
+	private AbstractConnection connection = new AbstractConnection() {
+
+		@Override
+		public String getDatabasePassword() {
+			return "";
+		}
+
+		@Override
+		public String getDatabaseUser() {
+			return "sa";
+		}
+
+		@Override
+		public String getDriver() {
+			return "org.h2.Driver";
+		}
+	};
+
+	private void close(PreparedStatement prepareStatement) {
+		try {
+			prepareStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void close(ResultSet resultSet) {
+		try {
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void insert(Console consoleForm) {
+		Connection conn = connection.open(jdbcUrl);
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = conn.prepareStatement("INSERT INTO CONSOLE (name,company)" + "VALUES (?, ?)");
+			preparedStatement.setString(1, consoleForm.getName());
+			preparedStatement.setString(2, consoleForm.getCompany());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			connection.close(preparedStatement);
+		}
+
+		connection.close(conn);
+	}
+
+	public Console search(Console consoleForm) {
+		Console consoleDB = null;
+		ResultSet resultSet = null;
+		PreparedStatement prepareStatement = null;
+		Connection conn = null;
+		try {
+			conn = connection.open(jdbcUrl);
+			prepareStatement = conn.prepareStatement("SELECT * FROM CONSOLE WHERE name = ?");
+			prepareStatement.setString(1, consoleForm.getName());
+			resultSet = prepareStatement.executeQuery();
+			while (resultSet.next()) {
+				consoleDB = new Console();
+				consoleDB.setName(resultSet.getString(0));
+				consoleDB.setCompany(resultSet.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			close(resultSet);
+			close(prepareStatement);
+
+		}
+		connection.close(conn);
+		return consoleDB;
+	}
+
+	public List<Console> searchAll() {
+		List<Console> games = new ArrayList<Console>();
+		Connection conn = connection.open(jdbcUrl);
+		ResultSet resultSet = null;
+		PreparedStatement prepareStatement = null;
+		try {
+			prepareStatement = conn.prepareStatement("SELECT * FROM CONSOLE");
+			resultSet = prepareStatement.executeQuery();
+			while (resultSet.next()) {
+				Console consoleDB = new Console();
+				consoleDB.setName(resultSet.getString(1));
+				consoleDB.setCompany(resultSet.getString(2));
+
+				games.add(consoleDB);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			close(resultSet);
+			close(prepareStatement);
+		}
+
+		connection.close(conn);
+		return games;
+	}
+
+	public void update(Console console) {
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			conn = connection.open(jdbcUrl);
+			preparedStatement = conn
+					.prepareStatement("UPDATE CONSOLE SET " + "name = ?, company = ? WHERE name = ?");
+			preparedStatement.setString(1, console.getName());
+			preparedStatement.setString(2, console.getCompany());
+			preparedStatement.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			connection.close(preparedStatement);
+			connection.close(conn);
+		}
+	}
 
 }
--- ASanmartin/src/main/java/repositories/GamesRepository.java
+++ ASanmartin/src/main/java/repositories/GamesRepository.java
@@ -1,5 +1,151 @@
 package repositories;
 
-public class GamesRepository {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import connections.AbstractConnection;
+import models.Game;
+
+public class GamesRepository {
+	
+	private static final String jdbcUrl = "jdbc:h2:file:./src/main/resources/test;INIT=RUNSCRIPT FROM 'classpath:scripts/VideoGame.sql'";
+
+	private AbstractConnection connection = new AbstractConnection() {
+
+		@Override
+		public String getDatabasePassword() {
+			return "";
+		}
+
+		@Override
+		public String getDatabaseUser() {
+			return "sa";
+		}
+
+		@Override
+		public String getDriver() {
+			return "org.h2.Driver";
+		}
+	};
+
+	private void close(PreparedStatement prepareStatement) {
+		try {
+			prepareStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void close(ResultSet resultSet) {
+		try {
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void insert(Game gameForm) {
+		Connection conn = connection.open(jdbcUrl);
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = conn.prepareStatement("INSERT INTO GAME (title,pegi,releaseDate)" + "VALUES (?, ?, ?)");
+			preparedStatement.setString(1, gameForm.getTitle());
+			preparedStatement.setString(2, gameForm.getAge());
+			preparedStatement.setDate(3, (Date) gameForm.getReleaseDate());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			connection.close(preparedStatement);
+		}
+
+		connection.close(conn);
+	}
+
+	public Game search(Game gameForm) {
+		Game gameDB = null;
+		ResultSet resultSet = null;
+		PreparedStatement prepareStatement = null;
+		Connection conn = connection.open(jdbcUrl);
+		try {
+			prepareStatement = conn.prepareStatement("SELECT * FROM GAME WHERE title = ?");
+			prepareStatement.setString(1, gameForm.getTitle());
+			resultSet = prepareStatement.executeQuery();
+			while (resultSet.next()) {
+				gameDB = new Game();
+				gameDB.setTitle(resultSet.getString(0));
+				gameDB.setAge(resultSet.getString(2));
+				gameDB.setReleaseDate(resultSet.getDate(3));
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			close(resultSet);
+			close(prepareStatement);
+
+		}
+		connection.close(conn);
+		return gameDB;
+	}
+
+	public List<Game> searchAll() {
+		List<Game> listGames = new ArrayList<Game>();
+		Connection conn = connection.open(jdbcUrl);
+		ResultSet resultSet = null;
+		PreparedStatement prepareStatement = null;
+		try {
+			prepareStatement = conn.prepareStatement("SELECT * FROM GAME");
+			resultSet = prepareStatement.executeQuery();
+			while (resultSet.next()) {
+				Game gameDB = new Game();
+				gameDB.setTitle(resultSet.getString(1));
+				gameDB.setAge(resultSet.getString(2));
+				gameDB.setReleaseDate(resultSet.getDate(3));
+
+				listGames.add(gameDB);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			close(resultSet);
+			close(prepareStatement);
+		}
+
+		connection.close(conn);
+		return listGames;
+	}
+
+	public void update(Game game) {
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			conn = connection.open(jdbcUrl);
+			preparedStatement = conn
+					.prepareStatement("UPDATE GAME SET " + "title = ?, age = ?, releaseDate = ? WHERE title = ?");
+			preparedStatement.setString(1, game.getTitle());
+			preparedStatement.setString(2, game.getAge());
+			preparedStatement.setDate(3, (Date) game.getReleaseDate());
+			preparedStatement.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			connection.close(preparedStatement);
+			connection.close(conn);
+		}
+	}
 }
